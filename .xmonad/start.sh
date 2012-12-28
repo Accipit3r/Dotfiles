dcba808c8909cb5d85f41f80871dcd595d06ad52
Truemper <truemper.accipiter@googlemail.com>
2012-12-28 15:28:26 +0100

    xmonad added

---
+++ .xmonad/start.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+nm-applet &
+kupfer &
+dropboxd &
+guake &
+feh --bg-scale Dropbox/Best/02539_bearslife_1366x768.jpg &
+kalu &
+owncloud &
+trayer --edge top --widthtype percent --width 10 --height 20 --align left --margin 700&
